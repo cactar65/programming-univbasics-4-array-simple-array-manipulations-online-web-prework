1610b03b0206e9e7d6be978247129237d04dc72f
Nicholas Walton <cactar65@gmail.com>
2019-09-28 21:43:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,6 +26,10 @@ def using_concat (array1, array2)
   return array1.concat(array2)
 end
 
-def using_insert(array,string)
+def using_insert (array,string)
   return array.insert(4, string) 
 end 
+
+def using_uniq (array)
+  
+end
